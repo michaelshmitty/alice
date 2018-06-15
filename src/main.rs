1d4f549d8c4ed7eeebe4e2b20f2c725bef971921
Michael Smith <m@michaelsmith.be>
2018-06-15 16:18:14 +0200

    Basic texture rendering

--- src/main.rs
+++ src/main.rs
@@ -2,37 +2,13 @@ extern crate rand;
 extern crate sdl2;
 extern crate clap;
 
-use std::path::Path;
-use std::process;
-
 use sdl2::event::Event;
-use sdl2::image::{LoadTexture, INIT_PNG};
+use sdl2::render::TextureAccess;
 use sdl2::keyboard::Keycode;
-use sdl2::rect::Point;
-use sdl2::rect::Rect;
 use std::thread;
 use std::time::{Duration, Instant};
 use clap::{Arg, App};
 
-use rand::Rng;
-
-const SPRITE_SIZE: u32 = 38;
-const SPRITE_ZOOM: u32 = 4;
-const SPRITE_REST_OFFSET: i32 = 4 * SPRITE_SIZE as i32;
-
-const SPRITE_NORTH_OFFSET: i32 = 0;
-const SPRITE_WEST_OFFSET: i32 = 1 * SPRITE_SIZE as i32;
-const SPRITE_SOUTH_OFFSET: i32 = 2 * SPRITE_SIZE as i32;
-const SPRITE_EAST_OFFSET: i32 = 3 * SPRITE_SIZE as i32;
-
-const MOVEMENT_SPEED: f32 = 300.0;
-const ANIMATION_SPEED: i32 = 100;
-const FRAMES_PER_ANIM: i32 = 4;
-
-const FACING_NORTH: u32 = 0;
-const FACING_WEST: u32 = 1;
-const FACING_SOUTH: u32 = 2;
-const FACING_EAST: u32 = 3;
 
 // FPS and frame capping constants
 const TARGET_FRAME_RATE: u64 = 60;
@@ -94,7 +70,6 @@ fn main() {
     // Initialize SDL
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
-    let _image_context = sdl2::image::init(INIT_PNG).unwrap();
     let joystick_subsystem = sdl_context.joystick().unwrap();
     let mut _joystick = None;
 
@@ -120,43 +95,10 @@ fn main() {
     let mut timer = sdl_context.timer().unwrap();
 
     // Load textures
-    let player_texture = texture_creator
-        .load_texture(Path::new("assets/bunny.png"))
-        .unwrap();
-    let mut background_texture = texture_creator
-        .load_texture(Path::new("assets/background.png"))
-        .unwrap();
-    background_texture.set_alpha_mod(220);
-    let carrot_texture = texture_creator
-        .load_texture(Path::new("assets/carrot.png"))
-        .unwrap();
-
-    let mut player_x: f32 = window_width as f32 / 2.0;
-    let mut player_y: f32 = window_height as f32 / 2.0;
-
-    let mut d_player_x = 0.0;
-    let mut d_player_y = 0.0;
-    let mut player_direction = FACING_EAST;
-
-    let mut source_rect = Rect::new(
-        0,
-        SPRITE_REST_OFFSET + (SPRITE_SIZE * player_direction) as i32,
-        SPRITE_SIZE,
-        SPRITE_SIZE,
-    );
-    let mut dest_rect = Rect::from_center(
-        Point::new(player_x as i32, player_y as i32),
-        SPRITE_SIZE * SPRITE_ZOOM,
-        SPRITE_SIZE * SPRITE_ZOOM,
-    );
-
-    // Generate a random carrot
-    let carrot_x: i32 = rand::thread_rng().gen_range(64,
-                                                     window_width as i32 - 64);
-    let carrot_y: i32 = rand::thread_rng().gen_range(64,
-                                                     window_height as i32 - 64);
-
-    let carrot_rect = Rect::from_center(Point::new(carrot_x, carrot_y), 64, 64);
+    let mut texture = texture_creator.create_texture(None,
+                                                     TextureAccess::Streaming,
+                                                     window_width,
+                                                     window_height).unwrap();
 
     // Variables for calculating framerate
     let mut last_frame_end_time = Instant::now();
@@ -197,38 +139,30 @@ fn main() {
                     if axis_idx == 0 {
                         // LEFT pressed
                         if val == -32768 {
-                            source_rect.set_y(SPRITE_WEST_OFFSET);
-                            d_player_x = -MOVEMENT_SPEED;
-                            player_direction = FACING_WEST;
+                            println!("LEFT pressed");
                         }
                         // RIGHT pressed
                         else if val == 32767 {
-                            source_rect.set_y(SPRITE_EAST_OFFSET);
-                            d_player_x = MOVEMENT_SPEED;
-                            player_direction = FACING_EAST;
+                            println!("RIGHT pressed");
                         }
                         // LEFT and RIGHT released
                         else if val > -dead_zone && val < dead_zone {
-                            d_player_x = 0.0;
+                            println!("LEFT/RIGHT released");
                         }
                     }
 
                     if axis_idx == 1 {
                         // UP pressed
                         if val == -32768 {
-                            source_rect.set_y(SPRITE_NORTH_OFFSET);
-                            d_player_y = -MOVEMENT_SPEED;
-                            player_direction = FACING_NORTH;
+                            println!("UP pressed");
                         }
                         // DOWN pressed
                         else if val == 32767 {
-                            source_rect.set_y(SPRITE_SOUTH_OFFSET);
-                            d_player_y = MOVEMENT_SPEED;
-                            player_direction = FACING_SOUTH;
+                            println!("DOWN pressed");
                         }
                         // UP and DOWN released
                         else if val > -dead_zone && val < dead_zone {
-                            d_player_y = 0.0;
+                            println!("UP/DOWN released");
                         }
                     }
                 }
@@ -242,53 +176,10 @@ fn main() {
             }
         }
 
-        // Move player
-        let dt_for_frame = 1.0 / TARGET_FRAME_RATE as f32;
-
-        let new_player_x = player_x + (dt_for_frame * d_player_x);
-
-        if new_player_x >= 32.0 && new_player_x <= window_width as f32 - 32.0 {
-            player_x = new_player_x;
-        } else {
-            d_player_x = 0.0;
-            source_rect.set_x(0);
-        }
-
-        let new_player_y = player_y + (dt_for_frame * d_player_y);
-        if new_player_y >= 42.0 && new_player_y <= window_height as f32 - 42.0 {
-            player_y = new_player_y;
-        } else {
-            d_player_y = 0.0;
-            source_rect.set_x(0);
-        }
-
-        dest_rect.center_on(Point::new(player_x as i32, player_y as i32));
-
-        // Walking / eating animation
-        let ticks = timer.ticks() as i32;
-
-        if !(d_player_x == 0.0) || !(d_player_y == 0.0) {
-            source_rect.set_x(SPRITE_SIZE as i32 *
-                ((ticks / ANIMATION_SPEED) % FRAMES_PER_ANIM));
-        } else {
-            // Stop animation
-            source_rect.set_x(0);
-            source_rect.set_y(SPRITE_REST_OFFSET +
-                (SPRITE_SIZE * player_direction) as i32);
-        }
-
         canvas.clear();
 
-        // Copy the background frame to the canvas
-        canvas.copy(&background_texture, None, None).unwrap();
-
-        // Copy the carrot to the canvas
-        canvas.copy(&carrot_texture, None, carrot_rect).unwrap();
-
-        // Copy the player frame to the canvas
-        canvas
-            .copy(&player_texture, Some(source_rect), Some(dest_rect))
-            .unwrap();
+        // Copy the texture to the canvas
+        canvas.copy(&texture, None, None).unwrap();
 
         canvas.present();
 
